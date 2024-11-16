2fea1e389b07e393082a429b938fbd9bce741526
JuanGmz <juanalfredohd@gmail.com>
2024-11-15 21:09:34 -0600

    CSS al formulario del modal

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
-  import { ref } from 'vue'
+  import { ref, reactive } from 'vue'
   import Presupuesto from './components/Presupuesto.vue'
+  import Modal from './components/Modal.vue'
   import ControlPresupuesto from './components/ControlPresupuesto.vue'
   import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 
   // States
+  const modal = reactive({
+    mostrar: false,
+    animar: false
+  })
   const presupuesto = ref(0)
   const disponible = ref(0)
 
@@ -12,6 +17,16 @@
   const definirPresupuesto = (cantidad) => {
     presupuesto.value = cantidad
     disponible.value = cantidad
+  }
+
+  const mostrarModal = () => {
+    modal.mostrar = true
+    modal.animar = true
+  }
+
+  const ocultarModal = () => {
+    modal.mostrar = false
+    modal.animar = false
   }
 </script>
 
@@ -36,8 +51,17 @@
 
     <main v-if="presupuesto > 0">
       <div class="crear-gasto">
-        <img :src="iconoNuevoGasto" alt="icono nuevo gasto">
+        <img 
+          :src="iconoNuevoGasto" 
+          alt="icono nuevo gasto"
+          @click="mostrarModal"
+        >
       </div>
+
+      <Modal
+          v-if="modal.mostrar"
+          @ocultar-modal="ocultarModal"
+        />
     </main>
   </div>
 </template>
